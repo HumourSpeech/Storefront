d896795762cd7f4c8b507b2804c704c76837738d
HumourSpeech <mishranitin6076@gmail.com>
2024-06-14 03:21:26 +0530

    Added features in models

--- store/models.py
+++ store/models.py
@@ -7,10 +7,13 @@ class Promotion(models.Model):
     discout = models.FloatField()
 
 class Collection(models.Model):
-    title = models.CharField(max_length==255)
-    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+') #here we have 2 ways to avoid the reverse relationship bcz we will face name clash.
-                                                                                                            #related_name='different name for collection'.
-                                                                                                            # #"related_name='+'":-this tells django not to create reverse relationship.
+    title = models.CharField(max_length=255)
+
+    #here we have 2 ways to avoid the reverse relationship bcz we will face name clash.
+    #related_name='different name for collection'.
+    #"related_name='+'":-this tells django not to create reverse relationship.
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+') 
+    
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -18,7 +21,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    promotions = models.ManytoManyField(Promotion) #for many_to_many relationship
+    promotions = models.ManyToManyField(Promotion) #for many_to_many relationship
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -30,10 +33,10 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER,'Silver'),
         (MEMBERSHIP_GOLD,'Gold')
     ]
-    first_name = models.Charfield(max_length=255)
-    last_name = models.Charfield(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone = models.Charfield(max_length=255)
+    phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
@@ -49,19 +52,19 @@ class order(models.Model):
     ]
 
     placed_at = models.DateTimeField(auto_now_add=True)
-    payment_status = models.Charfield(max_lenght=1, choices=PAYMENT_CHOICES, default=PAYMENT_PENDING)
-    customer = models.ForiegnKey(Costumer , on_delete=models.PROTECT)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_CHOICES, default=PAYMENT_PENDING)
+    customer = models.ForeignKey(Customer , on_delete=models.PROTECT)
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeigKey(Product, on_delete=models.PROTECT)
+    order = models.ForeignKey(order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.OnetoOneField(Customer, on_delete=models.CASCADE, primary_key=True) #for one_to_one relationship
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True) #for one_to_one relationship
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE) #for one_to_many relationship
 
 class Cart(models.Model):
